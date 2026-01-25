3e01b7c176b2a8ec9e0ad823b2236fe6703fddf6
Martin Kersner <martin@bisonai.com>
2026-01-25 14:59:03 +0900

    feat: hugo

---
+++ static/js/grayscale.js
@@ -0,0 +1,16 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// remove the focused state after click,
+// otherwise bootstrap will still highlight the link
+$("a").mouseup(function(){
+    $(this).blur();
+})
